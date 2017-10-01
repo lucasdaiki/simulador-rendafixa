56b75b593ffa12efc82d09c4250bd28dbfc0a5c6
Lucas Daiki <lucasdaiki.dev@gmail.com>
2017-10-01 18:29:01 -0300

    Remove códigos comentados

--- scripts/simulate.js
+++ scripts/simulate.js
@@ -110,62 +110,6 @@
         .forEach(field => {
           fillField(field.fieldName, field.value)
         })
-
-      // let grossAmount = document.querySelector('[data-attr="grossAmount"]')
-      // grossAmount.textContent = ''
-      // grossAmount.appendChild(document.createTextNode(data.grossAmount))
-
-      // let grossAmountProfit = document.querySelector('[data-attr="grossAmountProfit"]')
-      // grossAmountProfit.textContent = ''
-      // grossAmountProfit.appendChild(document.createTextNode(data.grossAmountProfit))
-
-      // let investedAmount = document.querySelector('[data-attr="investedAmount"]')
-      // investedAmount.textContent = ''
-      // investedAmount.appendChild(document.createTextNode(data.investmentParameter.investedAmount))
-
-      // let tdGrossAmount = document.querySelector('td[data-attr="grossAmount"]')
-      // tdGrossAmount.textContent = ''
-      // tdGrossAmount.appendChild(document.createTextNode(data.grossAmount))
-
-      // let tdGrossAmountProfit = document.querySelector('td[data-attr="grossAmountProfit"]')
-      // tdGrossAmountProfit.textContent = ''
-      // tdGrossAmountProfit.appendChild(document.createTextNode(data.grossAmountProfit))
-
-      // let taxesAmount = document.querySelector('span[data-attr="taxesAmount"]')
-      // taxesAmount.textContent = ''
-      // taxesAmount.appendChild(document.createTextNode(data.taxesAmount))
-
-      // let taxesRate = document.querySelector('span[data-attr="taxesRate"]')
-      // taxesRate.textContent = ''
-      // taxesAmount.appendChild(document.createTextNode(` (${data.taxesRate} %)`))
-
-      // let netAmount = document.querySelector('td[data-attr="netAmount"]')
-      // netAmount.textContent = ''
-      // netAmount.appendChild(document.createTextNode(data.netAmount))
-
-      // let maturityDate = document.querySelector('td[data-attr="maturityDate"]')
-      // maturityDate.textContent = ''
-      // maturityDate.appendChild(document.createTextNode(convertDate(data.investmentParameter.maturityDate)))
-
-      // let maturityTotalDays = document.querySelector('td[data-attr="maturityTotalDays"]')
-      // maturityTotalDays.textContent = ''
-      // maturityTotalDays.appendChild(document.createTextNode(data.investmentParameter.maturityTotalDays))
-
-      // let monthlyGrossRateProfit = document.querySelector('td[data-attr="monthlyGrossRateProfit"]')
-      // monthlyGrossRateProfit.textContent = ''
-      // monthlyGrossRateProfit.appendChild(document.createTextNode(data.monthlyGrossRateProfit))
-
-      // let rate = document.querySelector('td[data-attr="rate"]')
-      // rate.textContent = ''
-      // rate.appendChild(document.createTextNode(data.investmentParameter.rate))
-
-      // let annualGrossRateProfit = document.querySelector('td[data-attr="annualGrossRateProfit"]')
-      // annualGrossRateProfit.textContent = ''
-      // annualGrossRateProfit.appendChild(document.createTextNode(data.annualGrossRateProfit))
-
-      // let rateProfit = document.querySelector('td[data-attr="rateProfit"]')
-      // rateProfit.textContent = ''
-      // rateProfit.appendChild(document.createTextNode(data.rateProfit))
     }
 
     function convertDate(inputFormat) {
