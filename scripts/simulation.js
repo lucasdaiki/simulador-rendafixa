ab466da7210e8d3736f05c25f6cb28abede03e79
Lucas Daiki <lucasdaiki.dev@gmail.com>
2017-10-02 20:22:52 -0300

    Refactoring + estado de loading...

--- scripts/simulation.js
+++ scripts/simulation.js
@@ -1,25 +1,34 @@
 (
     function() {
+        const todayElement = document.querySelector('span[data-attr="today"]')
+        
+        const simulationBoxElement = document.querySelector('.iz-box.simulation')
+        const resultBoxElement = document.querySelector('.iz-box.result')
+
+        const formElement = document.querySelector('.iz-form')
+        const simulateButtonElement = document.querySelector('.iz-btn-simular')
+        const againButtonElement = document.querySelector('.iz-btn-again')
+
+        const investedAmountElement = document.querySelector('input[name=investedAmount]')
+        const maturityDateElement = document.querySelector('input[name=maturityDate]')
+        const rateElement = document.querySelector('input[name=rate]')
+        
         function init() {
             const today = new Date()
-            const todayElement = document.querySelector('span[data-attr="today"]')
             todayElement.appendChild(document.createTextNode(simulator.dateParsers.fromAPI(today)))
         
-            document.querySelector('.iz-btn-simular').addEventListener('click', handleSimulate)
-            document.querySelector('.iz-form').addEventListener('submit', handleSimulate)
-            
-            document.querySelector('.iz-btn-again').addEventListener('click', function () {
-                document.querySelector('.iz-box.simulation').classList.add('active')
-                document.querySelector('.iz-box.result').classList.remove('active')
+            simulateButtonElement.addEventListener('click', handleSimulate)
+            formElement.addEventListener('submit', handleSimulate)
+
+            againButtonElement.addEventListener('click', function () {
+                simulationBoxElement.classList.add('active')
+                resultBoxElement.classList.remove('active')
             })
 
             setInputHandles()
         }
 
         function setInputHandles() {
-            const investedAmountElement = document.querySelector('input[name=investedAmount]')
-            const maturityDateElement = document.querySelector('input[name=maturityDate]')
-            const rateElement = document.querySelector('input[name=rate]')
 
             function validateForm() {
                 const today = moment(new Date())
@@ -27,10 +36,11 @@
                 const dateIsValid = maturityDateElement.value.length === 10 && today.isBefore(maturityDate)
                 const souldSubmit = investedAmountElement.value && rateElement.value && dateIsValid
 
-                if(souldSubmit)
-                    document.querySelector('.iz-btn-simular').removeAttribute('disabled')
+                if(souldSubmit){
+                    simulateButtonElement.removeAttribute('disabled')
+                }
                 else
-                    document.querySelector('.iz-btn-simular').setAttribute('disabled', true)
+                    simulateButtonElement.setAttribute('disabled', '')
             }
             
             investedAmountElement.addEventListener('keyup', validateForm)
@@ -69,10 +79,13 @@
 
         function handleSimulate (e) {
             e.preventDefault()
-            const investedAmount = document.querySelector('input[name=investedAmount]').value
-            const maturityDate = document.querySelector('input[name=maturityDate]').value
-            const rate = document.querySelector('input[name=rate]').value
-      
+            const investedAmount = investedAmountElement.value
+            const maturityDate = maturityDateElement.value
+            const rate = rateElement.value
+            
+            simulateButtonElement.setAttribute('disabled', '')
+            simulateButtonElement.classList.add('loading')
+
             axios.get(simulator.configs.URL_API_SIMULATOR, {
               params: {
                 investedAmount: investedAmount.replace(/\./g, '').replace(/\,/g, '.').replace('R$', '').trim(),
@@ -82,18 +95,17 @@
                 maturityDate:  simulator.dateParsers.toAPI(maturityDate)
               }
             })
-              .then(response => {
-                document.querySelector('.iz-box.simulation').classList.remove('active')
-                document.querySelector('.iz-box.result').classList.add('active')
-      
-                document.querySelector('input[name=investedAmount]').value = ''
-                document.querySelector('input[name=maturityDate]').value = ''
-                document.querySelector('input[name=rate]').value = ''
-      
+            .then(response => {
                 simulator.results.fillResults(response.data)             
-      
-              })
-              .catch(response => console.error(response))
+                resultBoxElement.classList.add('active')
+                
+                simulateButtonElement.classList.remove('loading')
+                simulationBoxElement.classList.remove('active')
+                investedAmountElement.value = ''
+                maturityDateElement.value = ''
+                rateElement.value = ''
+            })
+            .catch(response => console.error(response))
           }
 
         init()
