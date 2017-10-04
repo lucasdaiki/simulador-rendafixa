e9086034b839e9c72b568aab1b8b869b7f02f6e9
Lucas Daiki <lucasdaiki.dev@gmail.com>
2017-10-04 01:15:39 -0300

    Se dá erro ao calcular volta para index

--- scripts/results/results.js
+++ scripts/results/results.js
@@ -13,6 +13,7 @@
                 .then(fillFields)
                 .then(() => document.querySelector('.iz-box.result').classList.remove('blank'))
                 .then(() => document.querySelector('.iz-box.loading').classList.add('hide'))
+                .catch(() => window.location = '/')
         }
 
         const fillFields = (fieldsToFill) => {
